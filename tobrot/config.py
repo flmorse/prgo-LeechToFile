4e3823842f45b8ba292bf3b7c9803cced4015419
flmorse <69221003+flmorse@users.noreply.github.com>
2020-12-16 00:05:04 +0530

    Create config.py

---
+++ tobrot/config.py
@@ -0,0 +1,7 @@
+from tobrot.sample_config import Config
+
+class Config(Config):
+  TG_BOT_TOKEN = "1305064609:AAFPURHtYGZtr8A1gKk8U2v-KEGSdmRJhbw"
+  APP_ID = 735218
+  API_HASH = "5a401f985e2f2490685f5c4cefa77722"
+  AUTH_CHANNEL = [-1001318887404]
